60d7bc5093394cdb4789dd4105536a4a1e37b22e
Frederick Maclean <macleash90@gmail.com>
2020-09-03 17:31:54 +0000

    Initial Commit for Flutter Country Project

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,53 @@
-
-
 import 'package:flutter/material.dart';
 
-void main()
-{
-  runApp( new MaterialApp(
-    home: AllCountries() ,
-  ),
+void main() {
+  runApp(
+    new MaterialApp(
+      home: Home(),
+    ),
   );
 }
 
-class AllCountries extends StatelessWidget {
+//class AllCountries extends StatelessWidget {
+//  @override
+//
+//}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  var title;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    title = "Home";
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Countries All'),
-        backgroundColor: Colors.pink ,
-      ) ,
-      body: Container(
-        child: Text("Countries App"),
+        title: Text('Home'),
+        backgroundColor: Colors.pink,
       ),
-    )
-      ;
+      body: Container(
+          child: Column(
+        children: <Widget>[
+          Text(title),
+          RaisedButton(
+            onPressed: () {
+              setState(() {
+                title = "About Page";
+              });
+            },
+            child: Text('Button'),
+          )
+        ],
+      )),
+    );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:flutter_country_house/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(AllCountries());
+    await tester.pumpWidget(Home());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
